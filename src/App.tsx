23bc78ca774127cbc2c33e5f4da5aa4b92e1c004
Amy Peace <amypeace@172-26-37-73.eduroam.bath.ac.uk>
2026-03-28 21:42:34 +0000

    help page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Settings } from './pages/Settings';
 import { NotFound } from './pages/NotFound';
+<<<<<<< HEAD
 import WebCamMotionTrackerPage from './pages/WebCamMotionTracker';
+=======
+import Help from './pages/Help';
+>>>>>>> a9061d4 (help page)
 import BrainrotAttack from './components/BrainrotAttack';
 
 
@@ -32,10 +36,17 @@ function App() {
               Settings
             </Link>
             <Link
+<<<<<<< HEAD
               to="/webcam-motion-tracker"
               className="text-slate-200 hover:text-blue-300 font-medium transition-colors"
             >
               Webcam Motion Tracker
+=======
+              to="/help"
+              className="text-slate-200 hover:text-blue-300 font-medium transition-colors"
+            >
+              Help
+>>>>>>> a9061d4 (help page)
             </Link>
           </nav>
         </div>
@@ -47,6 +58,7 @@ function App() {
         <Route path="/settings" element={<Settings />} />
         <Route path="/webcam-motion-tracker" element={<WebCamMotionTrackerPage />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </div>
   );
